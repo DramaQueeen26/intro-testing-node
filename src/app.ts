fb18dad7d0d775429fcadedf3c320202d6818b62
Mariangel Yajure <mariangel.yajure@gmail.com>
2023-08-30 12:50:45 -0400

    Actualizando para iniciar el testing

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,48 @@
-import { findHeroById } from './services/hero.service';
+import { getPokemonById } from "./js-foundation/06-promises";
+import { buildLogger } from "./plugins/logger.plugin";
+// const { getAge, getUUID } = require('./plugins');
+
+
+// const { emailTemplate } = require('./js-foundation/01-template');
+// require('./js-foundation/02-destructuring');
+// const { getUserById } = require('./js-foundation/03-callbacks');
+// const { getUserById } = require('./js-foundation/04-arrow');
+// const { buildMakePerson } = require('./js-foundation/05-factory')
+
+
+// const getPokemonById = require('./js-foundation/06-promises');
+
+ 
+const logger = buildLogger('app.js');
+
+logger.log('Hola mundo');
+logger.error('Esto es algo malo');
+
+// getPokemonById(4)
+//   .then( ( pokemon ) => console.log({ pokemon }) )
+//   .catch( ( err ) => console.error( err ) )
+//   .finally( () => console.log('Finalmente') );
 
 
 
-const hero = findHeroById( 2 );
 
 
-console.log( hero?.name ?? 'No hero found'  ); // hero = undefined.name
+
+// token de acceso
+// Publicas
+
+
+// ! Referencia a la función factory y uso
+// const makePerson = buildMakePerson({ getUUID, getAge });
+
+// const obj = { name: 'John', birthdate: '1985-10-21' };
+
+// const john = makePerson( obj );
+
+// console.log({ john });
+
+
+
+
+
+
