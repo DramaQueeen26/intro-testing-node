019f052946c74f7d5c64d4a83aef352b73c013ee
Mariangel Yajure <mariangel.yajure@gmail.com>
2023-08-30 13:49:58 -0400

    Pruebas en 01-Template

---
+++ tests/js-foundation/01-template.test.ts
@@ -0,0 +1,19 @@
+import { emailTemplate } from '../../src/js-foundation/01-template';
+
+describe('js-foundation/01-template.ts', () => { 
+
+  test('emailTemplate should contain a greeting', () => { 
+
+    expect( emailTemplate ).toContain('Hi, ')
+
+   })
+
+   test('emailTemplate should contain {{ name }} and {{ orderId }}', () => { 
+
+    // * Expresiones regulares
+    expect( emailTemplate ).toMatch(/{{name}}/)
+    expect( emailTemplate ).toMatch(/{{orderId}}/)
+
+   })
+
+ })
